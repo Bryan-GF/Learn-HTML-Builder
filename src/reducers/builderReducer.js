c545c028cb9a010dbbe88c68ec9355d3ed77d8d0
Chiboba <briangfelix@gmail.com>
2019-07-16 12:35:28 -0700

    Basic setup with redux

---
+++ src/reducers/builderReducer.js
@@ -0,0 +1,16 @@
+import { 
+
+} from '../actions';
+
+const initialState = {
+    
+};
+
+
+export const builderReducer = (state = initialState, action) => {
+    switch (action.type) {
+        
+        default:
+            return state;
+    }
+  };
